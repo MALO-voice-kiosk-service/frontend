04d271ea33851a4176cb1d1acaa5949c6b949a72
sunnygeem <marguerite27@hanyang.ac.kr>
2024-07-29 20:32:10 +0900

    s_community_view/upload

---
+++ lib/screens/community/comm_upload.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class CommUploadPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => CommUploadPageState();
+}
+
+class CommUploadPageState extends State<CommUploadPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          toolbarHeight: 50,
+          automaticallyImplyLeading: false,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                '게시글 쓰기',
+                style: TextStyle(
+                  color: const Color(0xff481C75),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              IconButton(
+                  icon: const Icon(Icons.close),
+                  color: const Color(0xff481C75),
+                  onPressed: () {
+                    Get.back();
+                  }
+              ),
+            ],
+          ),
+        ),
+        body: Scaffold(
+          backgroundColor: Colors.white,
+          resizeToAvoidBottomInset : false,
+          body: SingleChildScrollView(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 40,
+                  height: MediaQuery.of(context).size.height - 100,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(
+                      color: Colors.black38,
+                    )
+                  ),
+                  child: SingleChildScrollView(
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '태그',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              ElevatedButton(
+                                  onPressed: null,
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(const Color(0xff481C75)),
+                                ),
+                                  child: const Text(
+                                      '친목',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                              ),
+                              const SizedBox(width: 5,),
+                              const ElevatedButton(
+                                onPressed: null,
+                                child: Text(
+                                  '홍보',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 5,),
+                          const Text(
+                            '제목',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextFormField(
+                          ),
+                          const SizedBox(height: 5,),
+                          const Text(
+                            '위치',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextFormField(
+                          ),
+                          const SizedBox(height: 5,),
+                          const Text(
+                            '사진',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 5,),
+                          ElevatedButton(
+                            onPressed: () async {
+                              var picker = ImagePicker();
+                              var image = await picker.pickImage(source: ImageSource.gallery);
+                            },
+                            child: const Text(
+                              '사진을 업로드해주세요',
+                              style: TextStyle(
+                                color: Colors.white70,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 5,),
+                          const Text(
+                            '본문',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 5,),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(30),
+                                border: Border.all(
+                                  color: Colors.black38,
+                                )
+                            ),
+                            width: MediaQuery.of(context).size.width - 60,
+                            height: 500,
+                            padding: EdgeInsets.all(20),
+                            child: const TextField(
+                              keyboardType: TextInputType.multiline,
+                              maxLines: null,
+                              decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sparcs_2024_frontend/screens/community/comm_view.dart';
 import 'package:sparcs_2024_frontend/screens/loading.dart';
 import 'package:sparcs_2024_frontend/screens/trail/trail_main.dart';
 
@@ -26,7 +27,7 @@ class MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  final List<Widget> _pages = [TrailMainPage(), TrailMainPage(), TrailMainPage(), TrailMainPage(), TrailMainPage()];
+  final List<Widget> _pages = [TrailMainPage(), TrailMainPage(), CommViewPage(), TrailMainPage(), TrailMainPage()];
 
   void _onItemTapped(int index) {
     setState(() {
