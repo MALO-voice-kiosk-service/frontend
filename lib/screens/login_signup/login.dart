045f115a25a7f6ec4921c1c65c6cd00ab4ac4105
sunnygeem <marguerite27@hanyang.ac.kr>
2024-07-30 11:28:17 +0900

    s_login_loginAPI

--- lib/screens/login_signup/login.dart
+++ lib/screens/login_signup/login.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:sparcs_2024_frontend/screens/login_signup/signup.dart';
-import 'package:sparcs_2024_frontend/screens/trail/trail_main.dart';
-
+import '../../service/api.dart';
 import '../main.dart';
 
 class LoginPage extends StatefulWidget {
@@ -12,7 +10,62 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
+  final TextEditingController userIdController = TextEditingController();
+  String userId = "";
 
+  final TextEditingController userPwController = TextEditingController();
+  String userPw = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    // TextEditingController에 리스너를 추가하여 입력 값 변경 시 userId 변수 업데이트.
+    userIdController.addListener(() {
+      setState(() {
+        userId = userIdController.text;
+        print("User ID: $userId"); // 디버깅용 로그
+      });
+    });
+    // TextEditingController에 리스너를 추가하여 입력 값 변경 시 userPw 변수 업데이트.
+    userPwController.addListener(() {
+      setState(() {
+        userPw = userPwController.text;
+        print("User PW: $userPw"); // 디버깅용 로그
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // 사용이 끝나면 TextEditingController를 dispose.
+    userIdController.dispose();
+    userPwController.dispose();
+    super.dispose();
+  }
+
+  // 로그인 버튼 클릭 시 호출될 함수
+  Future<void> _login() async {
+    // API URL을 정의합니다.
+    const String url = '/api/user/login';
+
+    // 요청에 포함할 데이터를 정의합니다.
+    Map<String, dynamic> data = {
+      'userid': userId,
+      'password': userPw,
+    };
+
+    // httpPOST 함수를 호출하여 데이터를 전송합니다.
+    final response = await httpPost(path: url, data: data);
+
+    if (response == 200) {
+      // 로그인 성공 시 처리
+      Get.to(() => MainPage());
+    } else {
+      // 로그인 실패 시 처리
+      print('*** Login failed ***');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +83,8 @@ class LoginPageState extends State<LoginPage> {
                 alignment: Alignment.topCenter,
                 child: Image.asset(
                   'lib/assets/logo.png',
-                  width: (MediaQuery.of(context).size.width)*(2/3),
-                  height: (MediaQuery.of(context).size.width)*(1/5),
+                  width: (MediaQuery.of(context).size.width) * (2 / 3),
+                  height: (MediaQuery.of(context).size.width) * (1 / 5),
                   fit: BoxFit.contain,
                 ),
               ),
@@ -39,9 +92,14 @@ class LoginPageState extends State<LoginPage> {
                 height: 50,
               ),
               Container(
-                margin: EdgeInsets.fromLTRB((MediaQuery.of(context).size.width)*(1/5), 0, (MediaQuery.of(context).size.width)*(1/5), 0),
+                margin: EdgeInsets.fromLTRB(
+                    (MediaQuery.of(context).size.width) * (1 / 5),
+                    0,
+                    (MediaQuery.of(context).size.width) * (1 / 5),
+                    0),
                 child: TextFormField(
                   key: const ValueKey(3),
+                  controller: userIdController, // TextFormField에 controller를 설정.
                   decoration: const InputDecoration(
                     prefixIcon: Icon(
                       Icons.person,
@@ -74,9 +132,14 @@ class LoginPageState extends State<LoginPage> {
                 height: 40,
               ),
               Container(
-                margin: EdgeInsets.fromLTRB((MediaQuery.of(context).size.width)*(1/5), 0, (MediaQuery.of(context).size.width)*(1/5), 0),
+                margin: EdgeInsets.fromLTRB(
+                    (MediaQuery.of(context).size.width) * (1 / 5),
+                    0,
+                    (MediaQuery.of(context).size.width) * (1 / 5),
+                    0),
                 child: TextFormField(
                   key: const ValueKey(4),
+                  controller: userPwController, // TextFormField에 controller를 설정.
                   decoration: const InputDecoration(
                     prefixIcon: Icon(
                       Icons.lock,
@@ -109,57 +172,66 @@ class LoginPageState extends State<LoginPage> {
                 height: 40,
               ),
               Container(
-                margin: EdgeInsets.fromLTRB((MediaQuery.of(context).size.width)*(1/5), 0, (MediaQuery.of(context).size.width)*(1/5), 0),
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Get.to(() => MainPage());
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white, backgroundColor: Color.fromRGBO(139, 117, 181, 1.0),
-                    padding: EdgeInsets.symmetric(vertical: 15), // 버튼 높이 조정
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10), // 모서리를 둥글게 설정
-                    )
-                  ),
-                  child: const Text('로그인',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),),
-                )
-              ),
+                  margin: EdgeInsets.fromLTRB(
+                      (MediaQuery.of(context).size.width) * (1 / 5),
+                      0,
+                      (MediaQuery.of(context).size.width) * (1 / 5),
+                      0),
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _login(); // 로그인 함수 호출
+                    },
+                    style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Color.fromRGBO(139, 117, 181, 1.0),
+                        padding: EdgeInsets.symmetric(vertical: 15), // 버튼 높이 조정
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10), // 모서리를 둥글게 설정
+                        )),
+                    child: const Text(
+                      '로그인',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )),
               Container(
                 height: 40,
               ),
               Container(
-                  margin: EdgeInsets.fromLTRB((MediaQuery.of(context).size.width)*(1/5), 0, (MediaQuery.of(context).size.width)*(1/5), 0),
+                  margin: EdgeInsets.fromLTRB(
+                      (MediaQuery.of(context).size.width) * (1 / 5),
+                      0,
+                      (MediaQuery.of(context).size.width) * (1 / 5),
+                      0),
                   width: double.infinity,
                   child: ElevatedButton(
                     onPressed: () {
                       Get.to(() => SignupPage());
                     },
                     style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.black, backgroundColor: Colors.white,
-                        side: BorderSide(color: Color.fromRGBO(139, 117, 181, 1.0)),
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        side: BorderSide(
+                            color: Color.fromRGBO(139, 117, 181, 1.0)),
                         padding: EdgeInsets.symmetric(vertical: 15), // 버튼 높이 조정
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10), // 모서리를 둥글게 설정
-                        )
-                    ),
-                    child: const Text('회원가입',
+                        )),
+                    child: const Text(
+                      '회원가입',
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
-                      ),),
-                  )
-              ),
-
+                      ),
+                    ),
+                  )),
             ],
           ),
-
         ),
-      )
+      ),
     );
   }
 }
