7116bcaf25f892500035491b457d8f610dd96489
sunnygeem <marguerite27@hanyang.ac.kr>
2024-07-31 16:11:39 +0900

    s_trailpin

--- lib/screens/trail/trail_showpin.dart
+++ lib/screens/trail/trail_showpin.dart
@@ -13,6 +13,7 @@ class ShowTrailPinPage extends StatefulWidget {
 class ShowTrailPinPageState extends State<ShowTrailPinPage> {
   Future<void>? _initializeFuture;
   bool isDetailedMap = Get.arguments;
+  int locationTag = 0; // 화장실 : 0, 전동보장구 급속충전기 : 1, 영화관 : 2
 
   @override
   void initState() {
@@ -68,6 +69,128 @@ class ShowTrailPinPageState extends State<ShowTrailPinPage> {
       body: Stack(
         children: [
           NaverMapWidget(isDetailedMap: true),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            padding: EdgeInsets.only(left: 10, right: 10),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // 화장실 버튼
+                  ElevatedButton(
+                    onPressed: (){
+                      setState(() {
+                        locationTag = 0;
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.resolveWith<Color>((Set<WidgetState> states) {
+                        if(locationTag == 0){
+                          return const Color(0xff481C75);
+                        }
+                        return Colors.grey;
+                      }),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                            child: Image.asset(
+                                'lib/assets/toilet.png',
+                              fit: BoxFit.contain,
+                            ),
+                          width: 20,
+                          height: 20,
+                        ),
+                        SizedBox(width: 10,),
+                        const Text(
+                          '화장실',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 10,),
+                  // 충전기 버튼
+                  ElevatedButton(
+                    onPressed: (){
+                      setState(() {
+                        locationTag = 1;
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.resolveWith<Color>((Set<WidgetState> states) {
+                        if(locationTag == 1){
+                          return const Color(0xff481C75);
+                        }
+                        return Colors.grey;
+                      }),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          child: Image.asset(
+                            'lib/assets/wheelchair_2.png',
+                            fit: BoxFit.contain,
+                          ),
+                          width: 20,
+                          height: 20,
+                        ),
+                        SizedBox(width: 10,),
+                        const Text(
+                          '급속충전기',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 10,),
+                  // 영화관 버튼
+                  ElevatedButton(
+                    onPressed: (){
+                      setState(() {
+                        locationTag = 2;
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.resolveWith<Color>((Set<WidgetState> states) {
+                        if(locationTag == 2){
+                          return const Color(0xff481C75);
+                        }
+                        return Colors.grey;
+                      }),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          child: Image.asset(
+                            'lib/assets/movie.png',
+                            fit: BoxFit.contain,
+                          ),
+                          width: 20,
+                          height: 20,
+                        ),
+                        SizedBox(width: 10,),
+                        const Text(
+                          '영화관',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           Container(
             padding: EdgeInsets.only(bottom: 20),
             alignment: Alignment.bottomCenter,
