96c40fbba400bf4106b3147a42be36a53f952172
sunnygeem <marguerite27@hanyang.ac.kr>
2024-07-31 13:52:34 +0900

    s_traildetail/showpin_showpin/pagenavigation

--- lib/screens/trail/trail_showpin.dart
+++ lib/screens/trail/trail_showpin.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:developer';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:get/get.dart';
 import 'package:sparcs_2024_frontend/widgets/naver_map.dart';
 
 class ShowTrailPinPage extends StatefulWidget {
@@ -11,6 +12,7 @@ class ShowTrailPinPage extends StatefulWidget {
 
 class ShowTrailPinPageState extends State<ShowTrailPinPage> {
   Future<void>? _initializeFuture;
+  bool isDetailedMap = Get.arguments;
 
   @override
   void initState() {
@@ -32,7 +34,61 @@ class ShowTrailPinPageState extends State<ShowTrailPinPage> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: NaverMapWidget(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        toolbarHeight: 50,
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.close),
+              color: const Color(0xff481C75),
+              onPressed: () {
+                Get.back();
+              },
+              style: ButtonStyle(
+                overlayColor: WidgetStateColor.resolveWith((states) => Colors.transparent),
+              ),
+            ),
+            const Text(
+              '산책하기',
+              style: TextStyle(
+                color: Color(0xff481C75),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              width: 50,
+            ),
+          ],
+        ),
+      ),
+      body: Stack(
+        children: [
+          NaverMapWidget(isDetailedMap: true),
+          Container(
+            padding: EdgeInsets.only(bottom: 20),
+            alignment: Alignment.bottomCenter,
+            child: ElevatedButton(
+                onPressed: (){
+
+                },
+                child: const Text(
+                  '산책 끝내기',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                  ),
+                ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xff481C75),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
