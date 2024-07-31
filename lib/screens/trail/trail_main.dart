e0fac9b75dc1e59c62f7cdb90382b27d8a1e9c93
sunnygeem <marguerite27@hanyang.ac.kr>
2024-08-01 02:04:28 +0900

    s_trail_ui

--- lib/screens/trail/trail_main.dart
+++ lib/screens/trail/trail_main.dart
@@ -55,164 +55,184 @@ class TrailMainPageState extends State<TrailMainPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-        child: Column(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset(
+              'lib/assets/images/back_img.png',
+              fit: BoxFit.fill,
+            ),
+          ),
+          SingleChildScrollView(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: Column(
               children: [
-                SizedBox(height: 20),
-                Row(
+                Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: '느긋한 여유',
+                    const SizedBox(height: 50),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        RichText(
+                          text: const TextSpan(
+                            children: [
+                              TextSpan(
+                                text: '느긋한 여유',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'NanumSquareNeo',
+                                ),
+                              ),
+                              TextSpan(
+                                text: '를 선물할',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontFamily: 'NanumSquareNeo',
+                                ),
+                              ),
+                            ],
                             style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
+                              fontFamily: 'NanumSquareNeo',
+                            )
                           ),
-                          TextSpan(
-                            text: '를 선물할',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ],
+                        ),
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: const Text(
+                        '산책 루트를 안내합니다.',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
                       ),
                     ),
                   ],
                 ),
+                SizedBox(height: 10),
                 Container(
                   alignment: Alignment.topLeft,
                   child: const Text(
-                    '산책 루트를 안내합니다.',
+                    'yumin 님, 반가워요 !',
                     style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
+                      fontSize: 17,
                     ),
                   ),
                 ),
+                SizedBox(height: 40),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    '산책할 지역을 선택해주세요',
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  width: MediaQuery.of(context).size.width - 40,
+                  height: MediaQuery.of(context).size.height*(3/5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(
+                      color: Colors.black38,
+                      width: 3
+                    ),
+                  ),
+                  child: apiCityResult.isEmpty
+                      ? Center(child: CircularProgressIndicator())
+                      : ListView.builder(
+                    itemCount: apiCityResult.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final city = apiCityResult[index];
+                      final isSelected = selectedCities[index] ?? false;
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: OutlinedButton(
+                          onPressed: () {
+                            _toggleSelection(index);
+                          },
+                          style: OutlinedButton.styleFrom(
+                            side: BorderSide(
+                              color: isSelected ? const Color(0xff481C75) : Colors.white,
+                            ),
+                          ),
+                          child: Text(
+                            city['cityName'],
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  alignment: Alignment.bottomRight,
+                  margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {
+                      List<int> selectedCityIds = _getSelectedCityIds();
+                      print('여기서');
+                      print(selectedCityIds);
+                      Get.to(() => TrailMainPage2(), arguments: {'selectedCityIds': selectedCityIds});
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff481C75),
+                      foregroundColor: Colors.white,
+                      padding: EdgeInsets.only(left: 20, right: 20), // 버튼 높이 조정
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(45), // 모서리를 둥글게 설정
+                      ),
+                    ),
+                    child: const Text(
+                      '다음 단계로',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  width: MediaQuery.of(context).size.width - 40,
+                  height: MediaQuery.of(context).size.height * (1 / 7),
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(
+                      color: Colors.black38,
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '광고',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
               ],
             ),
-            SizedBox(height: 10),
-            Container(
-              alignment: Alignment.topLeft,
-              child: const Text(
-                'yumin 님, 반가워요 !',
-                style: TextStyle(
-                  fontSize: 17,
-                ),
-              ),
-            ),
-            SizedBox(height: 40),
-            Container(
-              alignment: Alignment.topLeft,
-              child: const Text(
-                '산책할 지역을 선택해주세요',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width - 40,
-              height: MediaQuery.of(context).size.height - 350,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(30),
-                border: Border.all(
-                  color: Colors.black38,
-                ),
-              ),
-              child: apiCityResult.isEmpty
-                  ? Center(child: CircularProgressIndicator())
-                  : ListView.builder(
-                itemCount: apiCityResult.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final city = apiCityResult[index];
-                  final isSelected = selectedCities[index] ?? false;
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: OutlinedButton(
-                      onPressed: () {
-                        _toggleSelection(index);
-                      },
-                      style: OutlinedButton.styleFrom(
-                        side: BorderSide(
-                          color: isSelected ? const Color(0xff481C75) : Colors.white,
-                        ),
-                      ),
-                      child: Text(
-                        city['cityName'],
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 17,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            SizedBox(height: 2),
-            Container(
-              alignment: Alignment.bottomRight,
-              margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () {
-                  List<int> selectedCityIds = _getSelectedCityIds();
-                  print('여기서');
-                  print(selectedCityIds);
-                  Get.to(() => TrailMainPage2(), arguments: {'selectedCityIds': selectedCityIds});
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: const Color(0xff481C75),
-                  padding: EdgeInsets.symmetric(vertical: 15), // 버튼 높이 조정
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // 모서리를 둥글게 설정
-                  ),
-                ),
-                child: const Text(
-                  '다음 단계로',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: MediaQuery.of(context).size.width - 40,
-              height: MediaQuery.of(context).size.height * (1 / 7),
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(30),
-                border: Border.all(
-                  color: Colors.black38,
-                ),
-              ),
-              child: Center(
-                child: Text(
-                  '광고',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/trail/trail_view.dart
+++ lib/screens/trail/trail_view.dart
@@ -28,93 +28,103 @@ class TrailViewPageState extends State<TrailViewPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        toolbarHeight: 50,
-        automaticallyImplyLeading: false,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: '느루',
-                    style: TextStyle(
-                      color: const Color(0xff481C75),
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  TextSpan(
-                    text: '가 추천하는 산책로입니다',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset(
+              'lib/assets/images/back_img.png',
+              fit: BoxFit.fill,
             ),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.only(top: 200, bottom: 200),
-              child: Row(
-                children: List.generate((apiMapResult!['data'] as List).length, (index) {
-                  return Container(
-                    width: MediaQuery.of(context).size.width * (1/2),
-                    height: MediaQuery.of(context).size.height * (1/3),
-                    margin: const EdgeInsets.only(right: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15),
-                      border: Border.all(
-                        color: Colors.black38,
+          ),
+          Column(
+            children: [
+              SizedBox(height: 80,),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '느루',
+                      style: TextStyle(
+                        color: const Color(0xff481C75),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: MediaQuery.of(context).size.width * (1/2) - 20,
-                                height: MediaQuery.of(context).size.height * (1/4),
-                                child: NaverMapWidget(isDetailedMap: false, lineString: apiMapResult!['data'][index]['cot_CONTS_GEOM'],)
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                //print(index);
-                                //print("선택된 walkway 아이디: ");
-                                //print(apiMapResult!['data'][index]['walkway_id']);
-                                Get.to(() => TrailDetailPage(), arguments: {apiMapResult!['data'][index]['walkway_id'].toString(), apiMapResult!['data'][index]['cot_CONTS_GEOM'].toString()});
-                              },
-                              child: Text(
-                                '상세 정보',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color(0xff481C75),
-                              ),
-                            ),
-                          ],
-                        ),
-                  );
-                }),
+                    TextSpan(
+                      text: '가 추천하는 산책로입니다',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                  style: TextStyle(
+                    fontFamily: 'NanumSquareNeo',
+                  ),
+                ),
               ),
-            ),
-          ],
-        ),
+              SingleChildScrollView(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  children: [
+                    SizedBox(height: 80,),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: List.generate((apiMapResult!['data'] as List).length, (index) {
+                          return Container(
+                            width: MediaQuery.of(context).size.width * (2/3),
+                            height: MediaQuery.of(context).size.height * (2/3),
+                            margin: const EdgeInsets.only(right: 10),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                              border: Border.all(
+                                color: Colors.black38,
+                              ),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      width: MediaQuery.of(context).size.width * (2/3) - 50,
+                                        height: MediaQuery.of(context).size.height * (2/3) - 100,
+                                        child: NaverMapWidget(isDetailedMap: false, lineString: apiMapResult!['data'][index]['cot_CONTS_GEOM'],)
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        //print(index);
+                                        //print("선택된 walkway 아이디: ");
+                                        //print(apiMapResult!['data'][index]['walkway_id']);
+                                        Get.to(() => TrailDetailPage(), arguments: {apiMapResult!['data'][index]['walkway_id'].toString(), apiMapResult!['data'][index]['cot_CONTS_GEOM'].toString()});
+                                      },
+                                      child: Text(
+                                        '상세 정보',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: const Color(0xff481C75),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                          );
+                        }),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
